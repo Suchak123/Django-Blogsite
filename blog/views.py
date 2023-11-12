95ab92f4ecbe311d82c1616b6d3ddcb0d466578b
Suchak123 <niraulasuchak@gmail.com>
2023-11-12 20:44:52 +0545

    pagination, password reset using class based view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView, 
     DetailView,
@@ -21,6 +24,20 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 4
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+        
+        
 
 class PostDetailView(DetailView):
     model = Post
@@ -60,6 +77,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 def about(request):
     context = {'title': 'About'}
-    return render(request, 'about.html', context)
+    return render(request, 'blog/about.html', context)
 
 # Create your views here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,10 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'niraulasuchak@gmail.com'
+EMAIL_HOST_PASSWORD = 'tsgb xbcl zvpl ecah'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
