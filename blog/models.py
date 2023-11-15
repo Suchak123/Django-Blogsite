893b5beea4bdf37da4f3ced91feacbad3875b53a
Suchak123 <niraulasuchak@gmail.com>
2023-11-15 08:24:05 +0545

    final commit

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)#date for creation
     author = models.ForeignKey(User , on_delete=models.CASCADE)
     date_modified = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.title
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,20 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = BASE_DIR / 'templates'
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#kk6$%1^e!197dbjidfyo)+s#-+7g%-dgp@o)u!=vr=)+t&t(x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -138,7 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'niraulasuchak@gmail.com'
-EMAIL_HOST_PASSWORD = 'tsgb xbcl zvpl ecah'
+EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_PW')
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
