5f29fb69c3ffc409e43ba31b159720289a4a9328
Suchak123 <niraulasuchak@gmail.com>
2023-11-16 09:37:13 +0545

    ...

---
+++ static_cdn/account/forms.py
@@ -0,0 +1,49 @@
+from typing import Any
+from django import forms
+# from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .models import Profile
+
+# class LoginForm(forms.Form):
+#     email = forms.CharField(max_length=50, required=True, label= "email", widget=forms.EmailInput(
+#         attrs= {'class' : 'form-control', 'placeholder': 'Enter email'}
+#     ), help_text='Required. Add a valid email address.')
+#     password = forms.CharField(max_length=50, widget=forms.PasswordInput(
+#         attrs={'class': 'form-control', 'placeholder':'Enter password',}
+#     ))
+
+
+class CreateUserForm(UserCreationForm):
+
+    email = forms.EmailField(widget=forms.EmailInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email' , 'password1', 'password2']
+
+
+# class UserAuthenticationForm(AuthenticationForm):
+
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+#     class Meta:
+#         model = User
+#         fields = ('email', 'password')    
+
+#     def clean(self) :
+#         if self.is_valid():
+#             email = self.cleaned_data['email']
+#             password = self.cleaned_data['password']
+#             if not authenticate(email=email, password=password):
+#                 raise forms.ValidationError("Invalid Login") 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(widget=forms.EmailInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']            
---
+++ static_cdn/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)#date for creation
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(auto_now=True)
+    
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
